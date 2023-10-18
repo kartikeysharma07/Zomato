9007de11f12eb462c6ce75865e2bacda113f3efb
kartikeysharma <kartikey.sharma@nagarro.com>
2023-10-18 23:00:22 +0530

    initial 3

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule,
